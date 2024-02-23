23c46aa73fab4037649252f7f81060feb73590ca
Fidel Hernandez <fidel.hdz@me.com>
2024-02-22 22:17:58 -0600

    Check email 213

--- public/assets/c0nt/contact_me.php
+++ public/assets/c0nt/contact_me.php
@@ -1,15 +1,17 @@
 	<?php
 	echo "Iniciando ";
 
+	$data = json_decode(file_get_contents('php://input'), true);
+
 	$to_Email   	= "fidel.hdz@me.com"; //Replace with recipient email address
 	$subject        = 'Mensaje desde el sitio ' . $_SERVER['SERVER_NAME']; //Subject line for emails
 
 	//Sanitize input data using PHP filter_var().
-	$user_Name      = $_POST['name'];
-	$user_Phone     = $_POST['phone'];
-	$user_Email     = $_POST['email'];
-	$user_Company   = $_POST['company'];
-	$user_Message   = $_POST['message'];
+	$user_Name      = $data['name'];
+	$user_Phone     = $data['phone'];
+	$user_Email     = $data['email'];
+	$user_Company   = $data['company'];
+	$user_Message   = $data['message'];
 
 	//proceed with PHP email.
 	$headers = 'From: '.$user_Email.'' . "\r\n" . 'Reply-To: no-reply@bjrmlogistics.com.mx' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
