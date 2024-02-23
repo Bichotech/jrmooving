f37f9347d95a97dec8df9ff456d87611e42f04ee
Fidel Hernandez <fidel.hdz@me.com>
2024-02-22 23:57:05 -0600

    Check email 213

--- public/assets/c0nt/contact_me.php
+++ public/assets/c0nt/contact_me.php
@@ -1,7 +1,7 @@
 <?php
 	$data = json_decode(file_get_contents('php://input'), true);
 
-	$to_Email   	= "fidel.hdz@me.com"; //Replace with recipient email address
+	$to_Email   	= "jfhdzp@gmail.com"; //Replace with recipient email address
 	$subject        = 'Mensaje desde el sitio ' . $_SERVER['SERVER_NAME']; //Subject line for emails
 
 	//Sanitize input data using PHP filter_var().
@@ -12,15 +12,15 @@
 	$user_Message   = $data['message'];
 
 	//proceed with PHP email.
-	$headers = 'From: '.$user_Email.'' . "\r\n" . 'Reply-To: no-reply@bjrmlogistics.com.mx' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+	$headers = 'From: ' . $user_Email . "\r\n" . 'Reply-To: no-reply@jrmlogistics.com.mx' . "\r\n" . 'Reply-To: no-reply@jrmlogistics.com.mx' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 
-	$body = 'Nombre: ' . $user_Name . "\r\n\n";
-	$body .= 'Teléfono: ' . $user_Phone . "\r\n";
-	if ( $user_Email != '' ) $body .= 'Email: ' . $user_Email . "\r\n";
-	if ( $user_Company != '' ) $body .= 'Compañía: ' . $user_Company . "\r\n";
-	if ( $user_Message != '' ) $body .= 'Mensaje: ' . "\r\n" . $user_Message . "\r\n";
+	$body = 'Nombre: ' . $user_Name . "<br>\r\n\n";
+	$body .= 'Teléfono: ' . $user_Phone . "<br>\r\n";
+	if ( $user_Email != '' ) $body .= 'Email: ' . $user_Email . "<br>\r\n";
+	if ( $user_Company != '' ) $body .= 'Compañía: ' . $user_Company . "<br>\r\n";
+	if ( $user_Message != '' ) $body .= 'Mensaje: ' . "\r\n" . $user_Message . "<br>\r\n";
 
-	echo $to_Email . "\n\r" . $subject . "\n\r" . $body . "\n\r" . $headers . "\n\r";
+	echo $to_Email . "<br>\n\r" . $subject . "<br>\n\r" . $body . "<br>\n\r" . $headers . "<br>\n\r";
 
 	$mailSent = @mail($to_Email, $subject, $body, $headers);
 
