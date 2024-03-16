a6c79c94d463da87e1b0c024d1daf92f4ed462dc
Fidel Hernandez <fidel.hdz@me.com>
2024-03-16 17:26:31 -0600

    Modificaciones al correo

--- public/assets/c0nt/contact.php
+++ public/assets/c0nt/contact.php
@@ -1,6 +1,6 @@
 <?php
 	$data = json_decode(file_get_contents('php://input'), true);
-	ini_set("include_path", '/home/jrmlogis/php:' . ini_get("include_path") );
+	ini_set("include_path", '/home/jrmlogis/php');
 
 	//Import PHPMailer classes into the global namespace
 	//These must be at the top of your script, not inside a function
@@ -8,11 +8,10 @@
 	use PHPMailer\PHPMailer\SMTP;
 	use PHPMailer\PHPMailer\Exception;
 
-	echo ini_get("include_path");
-
 	require ini_get("include_path") . '/PHPMailer/src/Exception.php';
 	require ini_get("include_path") . '/PHPMailer/src/PHPMailer.php';
 	require ini_get("include_path") . '/PHPMailer/src/SMTP.php';
+	require_once(ini_get("include_path") . '/define_vars.php');
 
 	//Create an instance; passing `true` enables exceptions
 	$mail = new PHPMailer(true);
@@ -23,7 +22,8 @@
 	$form_Company   = $data['company'];
 	$form_Message   = $data['message'];
 
-	$body = 'Nombre: ' . $form_Name . "<br>\r\n\n";
+	$body = "---------- <br>\r\n\n";
+	$body .= 'Nombre: ' . $form_Name . "<br>\r\n\n";
 	$body .= 'Teléfono: ' . $form_Phone . "<br>\r\n";
 	if ( $form_Email != '' ) {
 		$body .= 'Email: ' . $form_Email . "<br>\r\n";
@@ -32,23 +32,38 @@
 	}
 	if ( $form_Company != '' ) $body .= 'Compañía: ' . $form_Company . "<br>\r\n";
 	if ( $form_Message != '' ) $body .= 'Mensaje: ' . "\r\n" . $form_Message . "<br>\r\n";
+	$body .= "---------- <br>\r\n\n";
+
+	$body_alt = "---------- \r\n\n";
+	$body_alt .= 'Nombre: ' . $form_Name . "\r\n\n";
+	$body_alt .= 'Teléfono: ' . $form_Phone . "\r\n";
+	if ( $form_Email != '' ) {
+		$body_alt .= 'Email: ' . $form_Email . "\r\n";
+	} else {
+		$form_Email = 'no_email@jrmlogistics.com';
+	}
+	if ( $form_Company != '' ) $body_alt .= 'Compañía: ' . $form_Company . "\r\n";
+	if ( $form_Message != '' ) $body_alt .= 'Mensaje: ' . "\r\n" . $form_Message . "\r\n";
+	$body_alt .= "---------- \r\n\n";
 
 	try {
 		//Server settings
-		$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+		$mail->CharSet    = "UTF-8";
+		$mail->Encoding   = 'base64';
+		$mail->SMTPDebug  = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 		$mail->isSMTP();                                            //Send using SMTP
-		$mail->Host       = 'mail.jrmlogistics.com.mx';             //Set the SMTP server to send through
+		$mail->Host       = MAIL_SMTPSERVER;	            //Set the SMTP server to send through
 		$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-		$mail->Username   = 'it@jrmlogistics.com.mx';               //SMTP username
-		$mail->Password   = 'Perote_033';                           //SMTP password
+		$mail->Username   = MAIL_USERNAME;	                //SMTP username
+		$mail->Password   = MAIL_PASSWORD;                  //SMTP password
 		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-		$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+		$mail->Port       = MAIL_SMTPPORT;                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 		//Recipients
 		$mail->setFrom($form_Email);
-		$mail->addAddress('contacto@jrmlogistics.com.mx', 'JR Mooving Logistics');     //Add a recipient
-		$mail->addReplyTo('contacto@jrmlogistics.com.mx', 'Información');
-		$mail->addBCC('it@jrmlogistics.com.mx', 'Departamento de IT');
+		$mail->addAddress(MAIL_PRINCIPAL, 'JR Mooving Logistics');     //Add a recipient
+		$mail->addReplyTo(MAIL_PRINCIPAL, 'Información');
+		$mail->addBCC(MAIL_SECONDARY, 'Departamento de IT');
 
 		//Attachments
 		// $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
@@ -58,11 +73,11 @@
 		$mail->isHTML(true);                                  //Set email format to HTML
 		$mail->Subject = 'Mensaje para JR Mooving Logistics';
 		$mail->Body    = $body;
-		$mail->AltBody = $body;
+		$mail->AltBody = $body_alt;
 
 		$mail->send();
-		echo 'Mensaje enviado exitosamente';
+		echo 'Mensaje enviado exitosamente.';
 	} catch (Exception $e) {
-		echo "No se pudo enviar el mensaje. Errores de entrega: {$mail->ErrorInfo}";
+		echo "No se pudo enviar el mensaje.";
 	}
 ?>
